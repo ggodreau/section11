a07c067bff92286a7185950fd881b4e70cb4a3ec
ggodreau <godreau@gmail.com>
2017-05-15 15:48:33 +0100

    working default navbar

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -4,6 +4,7 @@ import {Injectable} from 'angular2/core';
 @Injectable()
 @Component({
     selector: 'my-navbar',
-    template: '<h1>myNavbar hello!</h1>'
+    //    template: '<h1>myNavbar hello!</h1>'
+    templateUrl: '/app/navbar.component.html'
 })
 export class MyNavbar { }
