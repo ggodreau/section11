88a65585e39ed2c764e1b4b99493c43f1f6f350c
ggodreau <godreau@gmail.com>
2017-05-15 16:49:10 +0100

    cleaned up nav bar, ready for primetime

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {MyNavbar} from '/app/navbar.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Section 11 Final, Initial Commit</h1><my-navbar></my-navbar>',
+    template: '<my-navbar></my-navbar>',
     directives: [MyNavbar]
 })
 export class AppComponent { }
