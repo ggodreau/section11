6ce4eb0ccc6fabda796ec0f59a11ffe456d4605a
ggodreau <godreau@gmail.com>
2017-05-23 18:22:14 +0100

    145 added create user http service (put)

--- app/users/newuser.component.ts
+++ app/users/newuser.component.ts
@@ -1,32 +1,38 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES, CanDeactivate} from 'angular2/router';
+import {ROUTER_DIRECTIVES, CanDeactivate, Router} from 'angular2/router';
 import {ControlGroup, FormBuilder, Validators} from 'angular2/common';
 import {CustomValidators} from '/app/shared/customvalidators';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {UsersService} from '/app/users/users.service';
 
 @Component({
     templateUrl: '/app/users/newuser.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [UsersService, HTTP_PROVIDERS]
 })
 export class NewUser implements CanDeactivate { 
     signupForm: ControlGroup;
     submitted = false;
 
-    constructor(fb: FormBuilder){
-        this.signupForm = fb.group({
-            name: ['', Validators.required],
-            email: ['', Validators.compose([
-                   Validators.required,
-                   CustomValidators.validateEmail,
-                   CustomValidators.isBlank
-                   ])],
-            phone: [],
-            address: fb.group({
-                street: [],
-                suite: [],
-                city: [],
-                zipcode: []
-            })
-        });
+    constructor(
+        fb: FormBuilder, 
+        private _usersService: UsersService,
+        private _router: Router){
+            this.signupForm = fb.group({
+                name: ['', Validators.required],
+                email: ['', Validators.compose([
+                       Validators.required,
+                       CustomValidators.validateEmail,
+                       CustomValidators.isBlank
+                       ])],
+                phone: [],
+                address: fb.group({
+                    street: [],
+                    suite: [],
+                    city: [],
+                    zipcode: []
+                })
+            });
     }
 
     log(x){
@@ -36,6 +42,12 @@ export class NewUser implements CanDeactivate {
     submit(){
     //        console.log(this.signupForm);
         this.submitted = true;
+        console.log(this.signupForm.value);
+        this._usersService.postUsers(this.signupForm.value)
+            .subscribe(x => { 
+                console.log("new ID of user is:", JSON.stringify(x));
+                console.log("returned");
+                this._router.navigate(['Users']);});
     }
 
     routerCanDeactivate(next, previous){
--- app/users/users.service.ts
+++ app/users/users.service.ts
@@ -14,4 +14,10 @@ export class UsersService {
         return this._http.get(this._url)
             .map(res => res.json());
     }
+
+    postUsers(users) {
+        return this._http.post(this._url, JSON.stringify(users))
+            .map(res => res.json());
+    }
+
 }
