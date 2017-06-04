fe405b9caf6eb5372ccb28822724ddf481c0273a
ggodreau <godreau@gmail.com>
2017-06-04 21:16:38 +0100

    151 got colspan working, needs http call for posts

--- app/posts/posts.component.ts
+++ app/posts/posts.component.ts
@@ -2,7 +2,25 @@ import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    template: '<h1>PostsComponent</h1>',
+    template: `
+        <h1>PostsComponent</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-6">
+                    <ul class="list-group">
+                        <li class="list-group-item">hello, world!</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    foobar
+                </div>
+            </div>
+        </div>
+        `
+        ,
     directives: [ROUTER_DIRECTIVES]
 })
-export class Posts { }
+export class Posts { 
+
+
+}
