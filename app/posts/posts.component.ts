836647c0463db4824a49ea6e6ab1bcc3d965ef22
ggodreau <godreau@gmail.com>
2017-06-06 00:47:25 +0100

    152 displaying list of posts in /posts

--- app/posts/posts.component.ts
+++ app/posts/posts.component.ts
@@ -1,5 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {PostsService} from '/app/posts/posts.service';
 
 @Component({
     template: `
@@ -8,19 +10,30 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
             <div class="row">
                 <div class="col-md-6 col-lg-6">
                     <ul class="list-group">
-                        <li class="list-group-item">hello, world!</li>
+                        <li *ngFor="#post of posts" class="list-group-item">{{ post.title }}</li>
                     </ul>
                 </div>
                 <div class="col-md-6 col-lg-3">
-                    foobar
+                    {{ posts | json }} 
                 </div>
             </div>
         </div>
         `
         ,
-    directives: [ROUTER_DIRECTIVES]
-})
-export class Posts { 
+    directives: [ROUTER_DIRECTIVES],
+    providers: [PostsService, HTTP_PROVIDERS]
 
+})
+export class Posts implements OnInit { 
+    posts: array;
+
+    constructor(private _postsService: PostsService){}
+
+    ngOnInit(){
+        this._postsService.getPosts()
+            .subscribe(res => {
+                console.log("posts: ", res);
+                this.posts = res;});
+    }
 
 }
---
+++ app/posts/posts.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PostsService {
+    private _url = "http://jsonplaceholder.typicode.com/posts";
+
+    constructor(private _http: Http){
+    }
+
+    getPosts() {
+        return this._http.get(this._url)
+            .map(res => res.json());
+    }
+}
