aedf0f7d315bd718d594c37f6ccc84f558a3ae06
ggodreau <godreau@gmail.com>
2017-06-08 23:39:36 +0100

    159 added dropdown, doesn't drop down doe

--- app/posts/posts.component.ts
+++ app/posts/posts.component.ts
@@ -10,6 +10,29 @@ import {Spinner} from '/app/shared/spinner.component';
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-lg-6">
+
+                    <!-- Posts -->
+
+                    <div class="dropdown">
+                        <button 
+                            class="btn btn-default dropdown-toggle"
+                            type="button" 
+                            id="dropdownMenu1"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="true">
+                            Dropdown
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+                
                     <spinner [isVisible]="postLoading"></spinner>
                     <ul *ngIf="!postLoading" class="list-group posts">
                         <li 
@@ -71,6 +94,7 @@ import {Spinner} from '/app/shared/spinner.component';
             border-radius: 100%;
             padding: 10px;
         }
+        .dropdown { position: relative; }
     `],
     directives: [ROUTER_DIRECTIVES, Spinner],
     providers: [PostsService, HTTP_PROVIDERS]
